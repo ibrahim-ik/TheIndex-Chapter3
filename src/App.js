91c66172819e3379e090d660ad9c039bb2ccbce3
ibrahim-ik <ibrahiim.alk@gmail.com>
2018-08-08 18:06:16 +0300

    finished TheIndex-Chapter3

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-
-// Data
-import authors from "./data";
-
+import axios from "axios";
+import cartoon_eye from "./cartoon_eye.jpg";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorsList from "./AuthorsList";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      authors: [],
+      loading: true
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://the-index-api.herokuapp.com/api/authors/")
+      .then(results => results.data)
+      .then(authors =>
+        this.setState({
+          authors: authors,
+          loading: false
+        })
+      );
+  }
   render() {
     return (
       <div id="app" className="container-fluid">
@@ -16,7 +32,11 @@ class App extends Component {
             <Sidebar />
           </div>
           <div className="content col-10">
-            <AuthorsList authors={authors} />
+            {this.state.loading ? (
+              <img src={cartoon_eye} />
+            ) : (
+              <AuthorsList authors={this.state.authors} />
+            )}
           </div>
         </div>
       </div>
